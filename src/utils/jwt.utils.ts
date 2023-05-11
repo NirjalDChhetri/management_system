b7215c85f9c32dac3df97b5a922ff055715e195a
NirjalDChhetri <nirjald3@gmail.com>
2023-05-11 23:29:18 +0545

    fixed: status code

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,12 +1,22 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
+import { IJwtPayload, IJwtOptions } from "../interfaces/jwt.interface";
 
 class JwtService {
-    static sign(payload: {id: string}, secret: string, expiresIn: string): string {
-        return jwt.sign(payload, secret, {
-            expiresIn
-        })
-    }
-    static verify (token: string, secret: string): any {
-        return jwt.verify(token, secret)
-    }
+  static sign(user: IJwtPayload, options: IJwtOptions, role?: string) {
+    return jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        role: role,
+      },
+      options.secret,
+      {
+        expiresIn: options.expiresIn,
+      }
+    );
+  }
+  static verify(token: string, secret: string): any {
+    return jwt.verify(token, secret);
+  }
 }
+export default JwtService;
