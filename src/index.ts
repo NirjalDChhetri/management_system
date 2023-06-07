f25d3b3eddd2f8b442d442ada168abc125390f7c
NirjalDChhetri <nirjald3@gmail.com>
2023-06-07 23:32:13 +0545

    routes

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { AppDataSource } from "./config/database.config";
 import DotenvConfig from "./config/env.config";
 import cors from "cors";
+import routes from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', routes)
 
 app.listen(DotenvConfig.PORT, () => {
   console.log(`Server has started ${DotenvConfig.PORT} 🚀`);
