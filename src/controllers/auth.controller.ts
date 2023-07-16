d83fd6df159a985c2cecf0b29719dae337c04fc1
NirjalD <nirjald3@gmail.com>
2023-07-16 23:03:37 +0545

    wip:Adminlogin

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,10 +4,13 @@ import { Role } from "../constant/enum";
 import JwtService from "../utils/jwt.utils";
 import HttpException from "../utils/HttpException";
 import messages from "../constant/messages";
+import tokenService from "../services/token.service";
+import { StatusCodes } from "../constant/statusCodes";
 
 class AuthController {
   async adminLogin(req: Request, res: Response) {
     const data = req.body;
+    const ONE_DAY_AFTER = new Date(Date.now() + 1 * 24 * 60 * 60 * 1000);
     const admin = await authService.adminLogin(data);
     let token;
     if (admin.role) {
@@ -15,6 +18,12 @@ class AuthController {
     } else {
       throw HttpException.internalServerError(messages["invalidCredentials"]);
     }
+    await tokenService.create(token, ONE_DAY_AFTER, admin);
+    res.status(StatusCodes.SUCCESS).json({
+      success: true,
+      accessToken: token,
+      message: messages["validLogin"],
+    });
   }
 }
 
