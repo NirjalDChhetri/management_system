ae6a952ed61e8f3c22b6cbdf92105c8be0251f9f
NirjalD <nirjald3@gmail.com>
2023-07-17 22:53:33 +0545

    admin

--- src/dtos/admin.dto.ts
+++ src/dtos/admin.dto.ts
@@ -1,20 +1,18 @@
-import { IsEnum, IsNotEmpty, IsOptional, Validate, } from "class-validator";
+import { IsEnum, IsNotEmpty, IsOptional, Validate } from "class-validator";
 import { IsEmailExist } from "../customs/isEmailExist";
 import { IsStrongPassword } from "../customs/passwordStrength";
 import { Role } from "../constant/enum";
-
-
 export class AdminDTO {
-    @IsNotEmpty()
-    @IsNotEmpty()
-    @Validate(IsEmailExist)
-    email: string
+  @IsNotEmpty()
+  @IsNotEmpty()
+  @Validate(IsEmailExist)
+  email: string;
 
-    @IsNotEmpty()
-    @Validate(IsStrongPassword)
-    password: string
+  @IsNotEmpty()
+  @Validate(IsStrongPassword)
+  password: string;
 
-    @IsOptional()
-    @IsEnum(Role)
-    role: Role
+  @IsOptional()
+  @IsEnum(Role)
+  role: Role;
 }
