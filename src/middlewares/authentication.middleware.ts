9d2f867be875193fb97b473fe5784bffcbe63065
NirjalD <nirjald3@gmail.com>
2023-08-15 00:26:24 +0545

    wip:auth

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -4,6 +4,9 @@ import messages from "../constant/messages";
 import JwtService from "../utils/jwt.utils";
 import { User } from "../entities/user.entity";
 import { AppDataSource } from "../config/database.config";
+import { Role } from "../constant/enum";
+import { Admin } from "../entities/admin.entity";
+import { StatusCodes } from "../constant/statusCodes";
 
 const authentication = async (
   req: Request,
@@ -15,28 +18,41 @@ const authentication = async (
   if (!token) {
     return next(HttpException.badRequest(messages["unAuthorized"]));
   }
-  let payload = JwtService.verify(
-    token,
-    process.env.ACCESS_TOKEN_SECRET as string
-  );
-  if (!payload) {
-    throw HttpException.unauthorized(messages["unAuthorized"]);
-  }
-  let userRepository = AppDataSource.getRepository(User);
   try {
-    const user = await userRepository.findOne({
-      where: {
-        id: payload?.id,
-      },
-    });
+    let payload = JwtService.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET as string
+    );
+    if (!payload) {
+      throw HttpException.unauthorized(messages["unAuthorized"]);
+    }
+    let user: any;
+    const { id, email, role } = payload;
+    switch (role) {
+      case Role.ADMIN:
+        user = await Admin.findOne({
+          where: {
+            id: id,
+          },
+        });
+        break;
+      case Role.USER:
+        user = await User.findOne({
+          where: {
+            id: id,
+          },
+        });
+    }
     if (!user) {
       throw HttpException.badRequest(messages["unAuthorized"]);
     }
-    req.user = user;
-    next();
-    
-  } catch (error) {
-    next(error);
+    const { password, createdAt, deletedAt, updatedAt, ...rest } = user;
+    req.user = rest;
+    return next();
+  } catch (err: any) {
+    return next(
+      new HttpException(messages["unAuthorized"], StatusCodes.BAD_REQUEST)
+    );
   }
 };
 
